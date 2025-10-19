56237483468ef49af128ef87b89c8e909518474e
mpd <mpdavy@gmail.com>
2025-10-19 14:16:40 -0600

    Updating limelight code to better match example code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
@@ -3,36 +3,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
-
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
 @TeleOp(name="LimelightTest", group="Linear Opmode")
 public class LimelightTest extends LinearOpMode {
-
-
-    public double getDistanceFromTag(double ta) {
-        double scale = 23851.19;
-        double distance = (scale / ta);
-        return distance;
-    }
     private ElapsedTime runtime = new ElapsedTime();
     private Limelight3A limelight;
-
     private double distance_g;
 
     @Override
@@ -41,39 +20,32 @@ public class LimelightTest extends LinearOpMode {
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
-        limelight.pipelineSwitch(0); // Switch to pipeline number 0 which is ID:21
+        limelight.pipelineSwitch(4); // Switch to pipeline number 4 which is ID:24
 
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            LLResult result = null;
-
-            result = limelight.getLatestResult();
+            LLResult result = limelight.getLatestResult();
             if (result != null && result.isValid()) {
-                double tx = result.getTx(); // How far left or right the target is (degrees)
-                double ty = result.getTy(); // How far up or down the target is (degrees)
-                double ta = result.getTa(); // How big the target looks (0%-100% of the image)
-
-                telemetry.addData("Target X", tx);
-                telemetry.addData("Target Y", ty);
-                telemetry.addData("Target Area", ta);
-                Pose3D botpose = result.getBotpose();
-                distance_g = getDistanceFromTag(ta);
-                telemetry.addData( "Distance",distance_g);
-                if (botpose != null) {
-                    double x = botpose.getPosition().x;
-                    double y = botpose.getPosition().y;
-                    telemetry.addData("MT1 Location", "(" + x + ", " + y + ")");
-
+                Pose3D botpose = result.getBotpose_MT2();
+                distance_g = getDistanceFromTag(result.getTa());
+                telemetry.addData("Distance", distance_g);
+                telemetry.addData("Target X", result.getTx());
+                telemetry.addData("Target Y", result.getTy());
+                telemetry.addData("Target Area", result.getTa());
+                telemetry.addData("Botpose", botpose.toString());
                 telemetry.update();
             } else {
                 telemetry.addData("Limelight", "No Targets");
                 telemetry.update();
             }
         }
-
     }
-}
+    public double getDistanceFromTag(double ta) {
+        double scale = 23851.19;
+        double distance = (scale / ta);
+        return distance;
+    }
 }
