68105473216eec440f6ce0978cc972f26ad2d5b0
allly <allysmith64000@gmail.com>
2025-11-08 16:34:37 -0700

    trying to have auto center

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimelightTest.java
@@ -12,19 +12,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name="LimelightTest", group="Linear Opmode")
 public class LimelightTest extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
-    private Limelight3A limelight;
+    //private Limelight3A limelight;
     private double distance_g;
     private DcMotor turret = null;
+
+    private double turretPower = 0.0;
 
     @Override
     public void runOpMode() {
 
         turret = hardwareMap.get(DcMotor.class, "turret");
 
-        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
-        limelight.pipelineSwitch(4); // Switch to pipeline number 4 which is ID:24
+        limelight.pipelineSwitch(4); // Switch to pipeline number 4 which is ID:24*/
 
         waitForStart();
         runtime.reset();;
@@ -42,23 +44,32 @@ public class LimelightTest extends LinearOpMode {
                 telemetry.addData("Target X", result.getTy());
                 telemetry.addData("Target Y", result.getTx());
                 telemetry.addData("Target Area", result.getTa());
-                //telemetry.addData("Botpose", botpose.toString());
+                telemetry.addData("Botpose", botpose.toString());
 
                 telemetry.addData("MT1 Location", "("+ x +"," + y +")");
                 telemetry.update();
+
+
             } else {
                 telemetry.addData("Limelight", "No Targets");
                 telemetry.update();
             }
+            turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            turret.setDirection(DcMotor.Direction.FORWARD);
 
-            if (gamepad1.x) {
-                turret.setDirection(DcMotor.Direction.FORWARD);
-                turret.setPower(0.1);
-            } else if (gamepad1.b) {
-                turret.setDirection(DcMotor.Direction.REVERSE);
-                turret.setPower(0.1);
+
+            if (gamepad1.b) {
+                turret.setPower(0.6);
+            } else if (gamepad1.x) {
+                turret.setPower(-0.6);
             } else {
                 turret.setPower(0);
+                result = limelight.getLatestResult();
+                if (result != null && result.isValid()) {
+                    turretPower = getTurretPower(result.getTx());
+                    turret.setPower(turretPower);
+
+                }
             }
 
         }
@@ -71,4 +82,8 @@ public class LimelightTest extends LinearOpMode {
         return distance;
     }
 
+    public double getTurretPower (double tx){
+        double power = tx * 0.0023 + 0.4;
+        return power;
+    }
 }
