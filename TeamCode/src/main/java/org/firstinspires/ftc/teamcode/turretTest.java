1383b8ca63b049794474680ee2319b32f1c4368f
allly <allysmith64000@gmail.com>
2025-12-06 11:05:53 -0700

    Turret class work 12/6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.teamcode.yise.Turret;
+import org.firstinspires.ftc.teamcode.yise.Turret.turretMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -18,14 +19,20 @@ public class turretTest extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            if (gamepad1.b) {
-                turret.manualTurn(Turret.turretDirection.LEFT);
-            } else if (gamepad1.x){
-                turret.manualTurn(Turret.turretDirection.RIGHT);
-            } else {
-                turret.stop();
-            }
-
+           if (gamepad1.right_bumper && (turret.mode == turretMode.AUTO)) {
+               turret.changeMode();
+           } else if (gamepad1.right_bumper && (turret.mode == turretMode.MANUAL)) {
+               turret.autoMode();
+           }
+           if (turret.mode == turretMode.MANUAL) {
+               if (gamepad1.dpad_left) {
+                   turret.manualMode(Turret.turretDirection.LEFT);
+               } else if (gamepad1.dpad_right) {
+                   turret.manualMode(Turret.turretDirection.RIGHT);
+               } else {
+                   turret.stop();
+               }
+           }
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yise/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/yise/Turret.java
@@ -7,24 +7,47 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Turret {
 
+    LLResult result = null;
+    public static double myOffset = 0.2;
+    public static double mySlope = 0.2;
+    private double turretPower = 0.0;
+    private double myTy = 0.0;
     public DcMotor turret;
+    public Limelight3A limelight;
     public enum turretDirection {
         LEFT,
         RIGHT,
+        STOP,
     }
+    public enum turretMode{
+        AUTO,
+        MANUAL,
+    }
+    public turretMode mode;
 
     public Turret (HardwareMap hardwareMap){
         turret = hardwareMap.get(DcMotor.class, "turret");
         turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         turret.setDirection(DcMotor.Direction.REVERSE);
 
-        Limelight3A limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
         limelight.setPollRateHz(100); // This sets how often we ask Limelight for data (100 times per second)
         limelight.start(); // This tells Limelight to start looking!
         limelight.pipelineSwitch(4); // Switch to pipeline number 4 which is ID:24
+        mode = turretMode.MANUAL;
     }
 
-    public void manualTurn(turretDirection direction) {
+    public void changeMode() {
+        if (mode == turretMode.AUTO) {
+            mode = turretMode.MANUAL;
+        } else-if (mode == turretMode.MANUAL) {
+            mode = turretMode.AUTO;
+        }
+    }
+    public void manualMode(turretDirection direction) {
+        if (mode == turretMode.AUTO) {
+            mode = turretMode.MANUAL;
+        }
         switch (direction) {
             case LEFT:
                 turret.setPower(-0.7);
@@ -32,9 +55,38 @@ public class Turret {
             case RIGHT:
                 turret.setPower(0.7);
                 break;
+            case STOP:
+                turret.setPower(0);
+                break;
         }
+    }
+    public void autoMode(){
+        if (mode == turretMode.MANUAL){
+            mode = turretMode.AUTO;
+        }
+        result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            myTy = result.getTy();
+            turretPower = getTurretPower(myTy, myOffset, mySlope);
+            //telemetry.addData("Power=", turretPower);
+            //telemetry.addData("Ty=", myTy);
+            //telemetry.update();
+        } else {
+            turretPower = 0;
+        }
+        turret.setPower(turretPower);
     }
     public void stop(){
         turret.setPower(0);
+    }
+    private double getTurretPower (double ty, double myOffset, double mySlope){
+        if (ty < 0) {
+            return -.2*(ty * mySlope + myOffset);
+        } else if (ty > 0){
+            return -.2*(ty * mySlope - myOffset);
+        }
+        else {
+            return 0;
+        }
     }
 }
