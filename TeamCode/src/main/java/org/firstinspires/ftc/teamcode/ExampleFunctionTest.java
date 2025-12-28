36fe53d016d63f843b237643ce38207cc19020f7
allly <allysmith64000@gmail.com>
2025-12-28 14:11:30 -0700

    Adding limelight to new code 12/28

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleFunctionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ExampleFunctionTest.java
@@ -5,14 +5,14 @@ public class ExampleFunctionTest {
     // this is the main function that runs when you run this program
     // right-click in this window and choose "Run 'ExampleFunctionTest.main()' with Coverage"
     public static void main(String[] args) {
-        double mySlope = 0.0235;
-        double myOffset = 0.05;
-        double ty = 1.124;
+        double mySlope = 0.44;
+        double myOffset = 0.2;
+        double tx = 0.0;
 
-        double myPower = getTurretPower(ty, mySlope, myOffset);
+        double myPower = getTurretPower(tx, mySlope, myOffset);
 
         // this is what will print out to the screen
-        System.out.println("ty=" + ty);
+        System.out.println("tx=" + tx);
         System.out.println("Power=" + myPower);
     }
 
@@ -27,4 +27,3 @@ public class ExampleFunctionTest {
         }
     }
 }
-
