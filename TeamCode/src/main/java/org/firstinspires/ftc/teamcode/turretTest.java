f053062e795e1df771d96012457c0f71b1974db7
allly <allysmith64000@gmail.com>
2025-12-30 17:46:12 -0700

    working on the limelight 12/30

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/turretTest.java
@@ -2,17 +2,20 @@ package org.firstinspires.ftc.teamcode;
 
 import org.firstinspires.ftc.teamcode.yise.Turret;
 import org.firstinspires.ftc.teamcode.yise.Turret.turretMode;
+
+import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name="turretTest", group="Linear Opmode")
 public class turretTest extends LinearOpMode {
     private ElapsedTime homingTimer = new ElapsedTime();
     private ElapsedTime snapTimer = new ElapsedTime();
-
+    private double distance_g;
     private boolean modeTogglePressed = false;
 
     private enum SnapState { INACTIVE, SNAPPING_LEFT, PUSHING_LEFT, SNAPPING_RIGHT, PUSHING_RIGHT, GOING_HOME, HOMING_ROUTINE }
@@ -25,6 +28,7 @@ public class turretTest extends LinearOpMode {
     @Override
     public void runOpMode() {
         Turret turret = new Turret(hardwareMap, Turret.turretAlliance.RED, telemetry);
+        turret.limelight.pipelineSwitch(2); // Switch to pipeline number 3 which is ID:20
 
         waitForStart();
 
@@ -50,14 +54,11 @@ public class turretTest extends LinearOpMode {
                 if (Math.abs(triggerPower) > 0.05) {
                     currentSnapState = SnapState.INACTIVE;
                     turret.manualControl(triggerPower);
-                }
-                else if (gamepad1.left_bumper && gamepad1.right_bumper) {
+                } else if (gamepad1.left_bumper && gamepad1.right_bumper) {
                     currentSnapState = SnapState.GOING_HOME;
-                }
-                else if (gamepad1.right_bumper && currentSnapState != SnapState.SNAPPING_RIGHT && currentSnapState != SnapState.PUSHING_RIGHT) {
+                } else if (gamepad1.right_bumper && currentSnapState != SnapState.SNAPPING_RIGHT && currentSnapState != SnapState.PUSHING_RIGHT) {
                     currentSnapState = SnapState.SNAPPING_RIGHT;
-                }
-                else if (gamepad1.left_bumper && currentSnapState != SnapState.SNAPPING_LEFT && currentSnapState != SnapState.PUSHING_LEFT) {
+                } else if (gamepad1.left_bumper && currentSnapState != SnapState.SNAPPING_LEFT && currentSnapState != SnapState.PUSHING_LEFT) {
                     currentSnapState = SnapState.SNAPPING_LEFT;
                 }
             }
@@ -65,14 +66,14 @@ public class turretTest extends LinearOpMode {
             // --- 3. STATE MACHINE ---
             if (turret.mode == turretMode.AUTO) {
                 turret.autoMode();
-                turret.limelight.pipelineSwitch(3); // Switch to pipeline number 3 which is ID:20
-            }
-            else if (currentSnapState == SnapState.HOMING_ROUTINE) {
-                if (gamepad1.share) {
+                turret.limelight.pipelineSwitch(4); // Switch to pipeline number 3 which is ID:20
+            } else if (currentSnapState == SnapState.HOMING_ROUTINE) {
+                if (gamepad1.touchpad) {
                     if (!turret.limit.getState()) {
                         turret.turret.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                         turret.turret.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
                         turret.manualControl(0.4);
+
                     } else {
                         double curvePower = 0.9 - (homingTimer.seconds() * 0.33);
                         turret.manualControl(Range.clip(curvePower, 0.4, 0.9));
@@ -81,8 +82,7 @@ public class turretTest extends LinearOpMode {
                     currentSnapState = SnapState.INACTIVE;
                     turret.stop();
                 }
-            }
-            else if (currentSnapState == SnapState.GOING_HOME) {
+            } else if (currentSnapState == SnapState.GOING_HOME) {
                 int currentPos = turret.turret.getCurrentPosition();
                 int error = CENTER_TARGET - currentPos;
                 if (Math.abs(error) > TOLERANCE) {
@@ -101,10 +101,12 @@ public class turretTest extends LinearOpMode {
                     snapTimer.reset();
                     currentSnapState = SnapState.PUSHING_RIGHT;
                 }
-            }
-            else if (currentSnapState == SnapState.PUSHING_RIGHT) {
+            } else if (currentSnapState == SnapState.PUSHING_RIGHT) {
                 if (snapTimer.seconds() < 1.0) turret.manualControl(0.5);
-                else { turret.stop(); currentSnapState = SnapState.INACTIVE; }
+                else {
+                    turret.stop();
+                    currentSnapState = SnapState.INACTIVE;
+                }
             }
             // SNAP LEFT
             else if (currentSnapState == SnapState.SNAPPING_LEFT) {
@@ -114,12 +116,13 @@ public class turretTest extends LinearOpMode {
                     snapTimer.reset();
                     currentSnapState = SnapState.PUSHING_LEFT;
                 }
-            }
-            else if (currentSnapState == SnapState.PUSHING_LEFT) {
+            } else if (currentSnapState == SnapState.PUSHING_LEFT) {
                 if (snapTimer.seconds() < 1.0) turret.manualControl(-0.5);
-                else { turret.stop(); currentSnapState = SnapState.INACTIVE; }
-            }
-            else if (currentSnapState == SnapState.INACTIVE && Math.abs(gamepad1.right_trigger - gamepad1.left_trigger) <= 0.05) {
+                else {
+                    turret.stop();
+                    currentSnapState = SnapState.INACTIVE;
+                }
+            } else if (currentSnapState == SnapState.INACTIVE && Math.abs(gamepad1.right_trigger - gamepad1.left_trigger) <= 0.05) {
                 turret.stop();
             }
 
@@ -128,9 +131,30 @@ public class turretTest extends LinearOpMode {
             telemetry.addLine("---------------");
             telemetry.addData("ENC", turret.turret.getCurrentPosition());
             telemetry.addData("STATE", currentSnapState);
-            telemetry.addData("MODE", turret.mode);
+            LLResult result = null;
+            result = turret.limelight.getLatestResult();
+            if (result != null && result.isValid()) {
+                    Pose3D botpose = result.getBotpose();
+                    double x = botpose.getPosition().x * 39.37;
+                    double y = botpose.getPosition().y * 39.37;
+                    distance_g = getDistanceFromPose(x, y);
+
+                    telemetry.addData("Distance", distance_g);
+
+                }
+
+                telemetry.addData("MODE", turret.mode);
                 telemetry.addData("id", turret.getID());
-            telemetry.update();
+                telemetry.update();
+
         }
+
     }
+        double getDistanceFromPose(double x, double y){
+            double a = Math.abs(55 - y);
+            double b = Math.abs(-58 - x);
+            double c_sqrd = Math.pow(a, 2) + Math.pow(b, 2);
+            double distance = Math.sqrt(c_sqrd);
+            return distance;
+        }
 }
